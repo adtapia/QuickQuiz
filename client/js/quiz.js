493dc647db70567496a2ad6e5c89cd7a6153cfa6
adtapia <adrianstapia1@gmail.com>
2025-01-17 09:24:22 -0500

    update

--- client/js/quiz.js
+++ client/js/quiz.js
@@ -1,4 +1,23 @@
+
+const fs = require("fs");
+
+const path = "../client/data/questions.json";
+
 let question_set = [];
 let current_questions = 0;
 let score = 0;
 let timer;
+
+function readUserDB() {
+    let data = fs.readFileSync(path, "utf-8");
+    return JSON.parse(data);
+}
+
+
+try {
+    let userDB = readUserDB();
+    console.log(userDB[2].question)
+} catch (err) {
+    console.error('Error reading file:', err);
+}
+
