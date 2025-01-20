bb6229315ad700a3f7e70b086ae0c77b666c4b78
adtapia <adrianstapia1@gmail.com>
2025-01-19 20:34:00 -0500

    New Folder

--- Project/routes/quiz.js
+++ Project/routes/quiz.js
@@ -2,18 +2,17 @@ var express = require('express');
 var router = express.Router();
 const fs = require("fs");
 
-const path = "../../client/data/questions.json";
+const path = "./routes/questions.json";
 
 let question_set = [];
 let score = 0;
 
-// Function to read the questions database
 function readUserDB() {
     let data = fs.readFileSync(path, "utf-8");
     return JSON.parse(data);
 }
 
-//fills question_set with unique random questions
+// Fill question_set with unique random questions
 try {
     let userDB = readUserDB();
     let max = userDB.length;
@@ -32,37 +31,36 @@ try {
 }
 
 
-
-// GET quiz route
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     const current_question = 0;
+    score = 0; 
     res.render('quiz', { question_set, current_question, score });
 });
 
-
-router.get('/next/:index', (req, res) => {
+// POST route to handle "Next Question"
+router.post('/quiz/:index', (req, res) => {
     const current_question = parseInt(req.params.index, 10);
-    const userAns = req.query.answer; 
+    const userAns = req.body.answer; 
 
-    // Check if the users answer is correct
-    if (userAns && userAns === question_set[current_question].correctAnswer) {
-        score++; 
+    // Check if the user's answer is correct
+    if (userAns && userAns === question_set[current_question].answer) {
+        score = score + 1;
     }
-
+    
     const next_question = current_question + 1;
 
     if (next_question < question_set.length) {
         res.render('quiz', { question_set, current_question: next_question, score });
     } else {
-      //if we finish all the questions we want to go to the result page
+
         res.redirect(`/result?score=${score}`);
     }
 });
 
-// Results page
+// Goes to Results page
 router.get('/result', (req, res) => {
-  const score = req.query.score; 
-  res.render('result', { score });
+    const score = req.query.score; 
+    res.render('result', { score });
 });
 
 module.exports = router;
