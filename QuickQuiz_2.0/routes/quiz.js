072f994560532ce8f0f6f64961c2e528f14b056c
adtapia <adrianstapia1@gmail.com>
2025-01-22 20:45:50 -0500

    update of Trivia API

--- QuickQuiz_2.0/routes/quiz.js
+++ QuickQuiz_2.0/routes/quiz.js
@@ -1,11 +1,68 @@
 var express = require('express');
 var axios = require("axios");
+const he = require('he');
 var router = express.Router();
 
 let question_set = [];
 let score = 0;
-let token =null;
+token =null;
 
+function shuffleArr(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const randomIndex = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[randomIndex]] = [arr[randomIndex], arr[i]];
+    }
+    return arr;
+}
+
+
+
+async function genquest(amount=10,category=9,difficulty="easy",){
+    const baseURL = "https://opentdb.com/api.php";
+    const type = "multiple";
+    
+     const response = await axios.get(
+                baseURL,
+                {
+                    params: {
+                        amount: amount,
+                        category: category,
+                        difficulty: difficulty,
+                        type: type,
+                    }
+                }
+            );
+           
+        
+            
+              
+            let temp=response.data.results;
+            let tempL= temp.length;
+            
+            for(let i=0;i<tempL;i++){
+            let temp=response.data.results[i];
+            temp.incorrect_answers.push(temp.correct_answer);
+
+            temp.incorrect_answers = shuffleArr(temp.incorrect_answers);
+
+            temp.correct_answer = String.fromCharCode(65 + temp.incorrect_answers.indexOf(temp.correct_answer));
+            }
+            
+            const temp_set = temp.map(item => ({
+                question: he.decode(item.question),
+                A: he.decode(item.incorrect_answers[0]),
+                B: he.decode(item.incorrect_answers[1]),
+                C: he.decode(item.incorrect_answers[2]),
+                D: he.decode(item.incorrect_answers[3]),
+                answer: he.decode(item.correct_answer)
+            }));
+
+
+
+        
+    return temp_set;
+        
+}
   
 // Get questions from the Trivia API; Ensuring when the user resets they are served a new batch of questions 
 
@@ -13,78 +70,13 @@ router.get('/', async function(req, res) {
 
     const current_question = 0;
     score = 0; 
-
-    // https://opentdb.com/api.php?amount=11&category=11&difficulty=medium&type=multiple
-    //When the Quiz page is reached we want to create a key for the user to ensure they do not get repeated questions in a given session 
-    if(token === null){
-        let temp = await axios.get("https://opentdb.com/api_token.php?command=request")
-        token= temp.data.token;
-    }
-    console.log(token);
-    //fill in if Code 4: Token Empty (restart token)
-
-    const baseURL = "https://opentdb.com/api.php";
-    const amount =10;
-    const category =12;
-    const difficulty= "medium";
-    const type = "multiple";
-    const response = await axios.get(
-                baseURL,
-                {
-                    params: {
-                        amount: amount,
-                        category: category,
-                        difficulty: difficulty,
-                        type: type
-                    }
-                }
-            );
-
-        //might have to but the token resest here bc this is when we call the api 
         
-    question_set=response.data.results;
-
-     console.log(question_set);
-            
-
-
+    question_set=await genquest();
+  
     res.render('quiz', { question_set, current_question, score });
 });
 
-//log 01/21 the text is not coming in how we want it to we will have to use a external library to solve this look at const he = require('he'); to do so
-//we would have to seeve thoruhg the code but it has to be done also we have to shuffle the answers with the wrong answers and compare strings instead of
-//letters
 
-
-
-// router.get('/weather', async function(req, res) {
-//     const appid = process.env.apikey;
-//     const baseURL = "https://api.openweathermap.org/data/2.5/weather";
-//     const lat = 40.73674;
-//     const lon = -73.8202;
-//     const units = "metric";
-
-
-//     const keywords = req.query.keywords;
-//     const response = await axios.get(
-//         baseURL,
-//         {
-//             params: {
-//                 lat: lat,
-//                 lon: lon,
-//                 units: units,
-//                 appid: appid
-//             }
-//         }
-//     );
-//     console.log(response.data);
-  
-//     res.render("index", {
-//         "title": response.data.name,
-//         "data": response.data.main.temp,
-//         "icon": `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-//     });
-// });
 
 // POST route to handle "Next Question"
 router.post('/:index', (req, res) => {
@@ -107,9 +99,9 @@ router.post('/:index', (req, res) => {
 });
 
 
-router.get('/result', (req, res) => {
-  const score = req.query.score; 
-  res.render('result', { score });
-});
+// router.get('/result', (req, res) => {
+//   const score = req.query.score; 
+//   res.render('result', { score });
+// });
 
 module.exports = router;
